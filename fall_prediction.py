2d7cf483660b2f6a53ce9b0d81f3d7d8a44fbd29
Ashade Samson <ashadesamson99@gmail.com>
2023-11-13 22:31:03 +0100

    updated streamlit code

--- fall_prediction.py
+++ fall_prediction.py
@@ -1,10 +1,26 @@
+import os
+import pandas as pd
+import numpy as np
 import pickle
 import streamlit as st
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.ensemble import RandomForestClassifier 
 
 
 
-pickle_in = open("random_forest.pkl","rb")
-classifier = pickle.load(pickle_in)
+file_path = "random_forest.pkl"
+classifier = None 
+
+
+if os.path.exists(file_path):
+    try:
+        with open(file_path, 'rb') as pickle_in:
+            classifier = pickle.load(pickle_in)
+    except Exception as e:
+        st.error(f"Error loading the model: {e}")
+else:
+    st.error(f"Model file not found: {file_path}")
+
 
 
 
@@ -26,15 +42,22 @@ def main():
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
-    distance = st.text_input("Distance","Type Here")
-    pressure = st.text_input("Pressure","Type Here")
-    hrv = st.text_input("HRV Level","Type Here")
-    sugarLevel = st.text_input("Sugar Level","Type Here")
-    sp02 = st.text_input("spO2","Type Here")
-    accelerometer = st.text_input("Accelerometer Reading","Type Here")
+    _distance = st.text_input("Distance", 0)
+    distance = float(_distance)
+    _pressure = st.text_input("Pressure", 0)
+    pressure = float(_pressure)
+    _hrv = st.text_input("HRV Level", 0)
+    hrv = float(_hrv)
+    _sugarLevel = st.text_input("Sugar Level", 0)
+    sugarLevel = float(_sugarLevel)
+    _sp02 = st.text_input("spO2", 0)
+    sp02 = float(_sp02)
+    _accelerometer = st.text_input("Accelerometer Reading", 0)
+    accelerometer = float(_accelerometer)
     result=""
     if st.button("Predict"):
-        result=predict_fall(distance,pressure,hrv,sugarLevel,sp02,accelerometer)
+        prediction = predict_fall(distance,pressure,hrv,sugarLevel,sp02,accelerometer)
+        result = "Fall" if prediction[0] == 1 else "No Fall"
     st.success('The output is {}'.format(result))
     if st.button("About"):
         st.text("Lets LEarn")
